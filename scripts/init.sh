73f30e50988447a331fb8280581215cad220e1fc
Jason Nam <contact@jasonnam.com>
2019-09-10 21:05:52 +0700

    Clean up comments

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,30 +1,19 @@
 #!/bin/bash -e
+
 DRIVER_NAME="binary"
 VERSION="0.1.3"
 BASE_URL="https://raw.githubusercontent.com/summonbin/binary"
 
-
-###################
-#### Arguments ####
-###################
-
+# Arguments
 INSTALL_PATH=$1
 SCHEME_PATH=$2
 DEFAULT_CACHE_PATH=$3
 
-
-######################
-#### Build driver ####
-######################
-
+# Build driver
 mkdir -p "$INSTALL_PATH/$DRIVER_NAME"
 curl -L "$BASE_URL/$VERSION/scripts/run.sh" -o "$INSTALL_PATH/$DRIVER_NAME/run.sh"
 
-
-######################
-#### Build scheme ####
-######################
-
+# Build scheme
 mkdir -p "$SCHEME_PATH/$DRIVER_NAME"
 if [ ! -f "$SCHEME_PATH/$DRIVER_NAME/cache" ]
 then
